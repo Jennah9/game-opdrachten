759225f8db82d14c406513a2bae28c05e1d3f1d5
Jennah9 <145122235+Jennah9@users.noreply.github.com>
2023-12-18 11:34:23 +0100

    Add files via upload

---
+++ H2O04.js
@@ -0,0 +1,33 @@
+var strand;
+var strandX;
+
+function preload() {
+  strand = loadImage("images/backgrounds/strand.jpg");
+}
+
+function setup() {
+  canvas = createCanvas(600,400);
+  canvas.parent('processing');
+  strandX=0;
+  fill('black');
+  textFont("Verdana");
+  textSize(14);  
+}
+
+function draw() {
+  background('grey');
+  image(strand,strandX,0);
+  image(strand,strandX + width,0);
+  image(strand,strandX - width,0);
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    strandX++;
+  }
+  else {
+    strandX--; 
+  }
+  text("strandX=" + strandX,20,20);
+  if (strandX < -strand.width||strandX > strand.width) {
+    strandX = 0;
+  }
+}
