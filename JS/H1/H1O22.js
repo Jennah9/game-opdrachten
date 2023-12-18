17534eb9e2c2d680e6cdd5d8096456b2bcfbd11b
Jennah9 <145122235+Jennah9@users.noreply.github.com>
2023-12-18 11:38:16 +0100

    Update H1O22.js

--- JS/H1/H1O22.js
+++ JS/H1/H1O22.js
@@ -12,6 +12,11 @@ function setup() {
 
 function draw() {
   background('lavender');
+  if (mouseX <= height / 2){
+    background('yellow');
+  } else {
+    background('white');
+  }
   fill('black');  
   
   mouseX=constrain(mouseX,25,width - 25);
@@ -19,37 +24,15 @@ function draw() {
 
   text("Beweeg de muis | x = " + round(mouseX) + " en y = " + round(mouseY),10,20);
   
-  if (mouseX >= width - 30) {
+  ellipse(width / 2,height / 2,100);
+  tekenJos(mouseX,mouseY,kleur);
+  afstand=dist(mouseX,mouseY,225,225);
+  text(round(afstand),10,40);
+  
+  if (mouseX >= width - 30 || mouseX <= width - 420 || mouseY >= height - 30 || mouseY <= height -420 || afstand <= 80) {
     kleur='red';
   }
   else {
     kleur='indianred';
   }
-  
-  ellipse(width / 2,height / 2,100);
-  tekenJos(mouseX,mouseY,kleur);
-  afstand=dist(315,105,225,225);
-  text(afstand,10,40);
-}
-
-function tekenJos(x,y,kleur) {
-  push();
-  translate(x,y);
-  scale(1); 
-  noStroke();
-  fill(kleur);
-  ellipse(0,0,50);
-  fill('slategray');
-  ellipse(-7,-10,17);
-  ellipse(7,-10,17);
-  fill('white');
-  ellipse(-7,-8,7,13);
-  ellipse(7,-8,7,13);
-  fill('orange');
-  ellipse(0,3,17);
-  stroke('slategray');
-  strokeWeight(3);
-  fill('white');
-  arc(0, 13, 26, 13, 0, PI, CHORD);
-  pop();
 }
